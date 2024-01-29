a7c509ae55c69bc494acb8228be5458df97d1a52
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-01-29 18:02:40 +0200

    added signup logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto/create-user.dto';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { AuthDto } from './dto/create-user.dto';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
-  signup(authDto: AuthDto) {
-    return { message: 'Sign up', dto: authDto };
+  constructor(private prisma: PrismaService) {}
+  async signup(authDto: AuthDto) {
+    try {
+      const hash = await argon2.hash(authDto.password);
+      const data = { ...authDto, password: hash };
+
+      const user = await this.prisma.user.create({
+        data,
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ConflictException('Email is already taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
