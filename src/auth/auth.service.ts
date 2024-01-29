b7b767427196422d9ff8caf942e8061beb19470b
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-01-29 14:05:32 +0200

    added auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  signup() {
+    return { message: 'Sign up' };
+  }
+
+  signin() {
+    return { message: 'Sign in' };
+  }
+}
